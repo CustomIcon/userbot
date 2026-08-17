4ae50c6f2a9dc7e44832740fd38247b974bb1e08
Abdulla Aidhaan <idhaan701@outlook.com>
2026-08-17 19:43:17 +0500

    fix: fix dictionary plugin for v2 AI and handle 502 errors :bug:

--- userbot/plugins/dictionary.py
+++ userbot/plugins/dictionary.py
@@ -10,7 +10,7 @@ from userbot.plugins.help import add_command_help
 
 @UserBot.on_message(filters.command(["define", "dict"], ".") & filters.me)
 async def define(bot: UserBot, message: Message):
-    """ Thank you Poki!!"""
+    """Thank you Poki!!"""
     cmd = message.command
 
     input_string = ""
@@ -24,78 +24,32 @@ async def define(bot: UserBot, message: Message):
         await message.delete()
         return
 
-    def combine(s_word, name):
-        w_word = f"**__{name.title()}__**\n"
-        for i in s_word:
-            if "definition" in i:
-                if "example" in i:
-                    w_word += (
-                            "\n**Definition**\n<pre>"
-                            + i["definition"]
-                            + "</pre>\n<b>Example</b>\n<pre>"
-                            + i["example"]
-                            + "</pre>"
-                    )
-                else:
-                    w_word += (
-                            "\n**Definition**\n" + "<pre>" + i["definition"] + "</pre>"
-                    )
+    def combine(meaning):
+        w_word = f"**__{meaning.get('partOfSpeech', 'word').title()}__**\n"
+        for i in meaning.get("definitions", []):
+            w_word += f"\n**Definition**\n`{i.get('definition', '')}`"
+            if i.get("example"):
+                w_word += f"\n**Example**\n`{i['example']}`"
+            if i.get("synonyms"):
+                w_word += f"\n**Synonyms:** `{', '.join(i['synonyms'])}`"
+            if i.get("antonyms"):
+                w_word += f"\n**Antonyms:** `{', '.join(i['antonyms'])}`"
         w_word += "\n\n"
         return w_word
 
     def out_print(word1):
         out = ""
-        if "meaning" in list(word1):
-            meaning = word1["meaning"]
-            if "noun" in list(meaning):
-                noun = meaning["noun"]
-                out += combine(noun, "noun")
-                # print(noun)
-            if "verb" in list(meaning):
-                verb = meaning["verb"]
-                out += combine(verb, "verb")
-                # print(verb)
-            if "preposition" in list(meaning):
-                preposition = meaning["preposition"]
-                out += combine(preposition, "preposition")
-                # print(preposition)
-            if "adverb" in list(meaning):
-                adverb = meaning["adverb"]
-                out += combine(adverb, "adverb")
-                # print(adverb)
-            if "adjective" in list(meaning):
-                adjec = meaning["adjective"]
-                out += combine(adjec, "adjective")
-                # print(adjec)
-            if "abbreviation" in list(meaning):
-                abbr = meaning["abbreviation"]
-                out += combine(abbr, "abbreviation")
-                # print(abbr)
-            if "exclamation" in list(meaning):
-                exclamation = meaning["exclamation"]
-                out += combine(exclamation, "exclamation")
-                # print(exclamation)
-            if "transitive verb" in list(meaning):
-                transitive_verb = meaning["transitive verb"]
-                out += combine(transitive_verb, "transitive verb")
-                # print(tt)
-            if "determiner" in list(meaning):
-                determiner = meaning["determiner"]
-                out += combine(determiner, "determiner")
-                # print(determiner)
-            if "crossReference" in list(meaning):
-                crosref = meaning["crossReference"]
-                out += combine(crosref, "crossReference")
-                # print(crosref)
+        for meaning in word1.get("meanings", []):
+            out += combine(meaning)
         if "title" in list(word1):
             out += (
-                    "**__Error Note__**\n\n▪️`"
-                    + word1["title"]
-                    + "\n\n▪️"
-                    + word1["message"]
-                    + "\n\n▪️<i>"
-                    + word1["resolution"]
-                    + "</i>`"
+                "**__Error Note__**\n\n▪️`"
+                + word1["title"]
+                + "\n\n▪️"
+                + word1["message"]
+                + "\n\n▪️<i>"
+                + word1["resolution"]
+                + "</i>`"
             )
         return out
 
@@ -103,9 +57,16 @@ async def define(bot: UserBot, message: Message):
         await message.edit("`Plz enter word to search‼️`")
     else:
         word = input_string
-        r_dec = await AioHttp().get_json(
-            f"https://api.dictionaryapi.dev/api/v1/entries/en/{word}"
-        )
+        try:
+            r_dec = await AioHttp().get_json(
+                f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+            )
+        except Exception as e:
+            print(e)
+            await message.edit(
+                "`The dictionary API is unreachable right now. Try again later.`"
+            )
+            return
 
         v_word = input_string
         if isinstance(r_dec, list):
